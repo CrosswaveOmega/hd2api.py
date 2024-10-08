bd5a25d9ea26879642399c0ced645254de81d078
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2024-10-08 09:42:07 -0400

    Update service_base.py

--- src/hd2api/services/service_base.py
+++ src/hd2api/services/service_base.py
@@ -19,7 +19,7 @@ hd2api_logger.setLevel(logging.INFO)
 async def make_async_api_request(
     base_path: str, path: str, api_config: APIConfig, params: Optional[dict] = None
 ) -> Any:
-    """Make a request to any endpoint."""
+    """Make a asyncronous request to any endpoint."""
 
     headers = {
         "Content-Type": "application/json",
@@ -38,7 +38,38 @@ async def make_async_api_request(
                 )  # Added params to the request
             else:
                 response = await client.get(path, headers=headers)  # Added params to the request
-    except httpx.ConnectError as e:
+    except httpx.HTTPError as e:
+        print(e)
+        hd2api_logger.error(str(e), exc_info=e)
+        raise e
+
+    if response.status_code != 200:
+        raise HTTPException(
+            response.status_code, f"Failed with status code: {response.status_code}"
+        )
+
+    data = response.json()
+    return data
+
+def make_sync_api_request(
+    base_path: str, path: str, api_config: APIConfig, params: Optional[dict] = None
+) -> Any:
+    """Make a synchronous request to any endpoint."""
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Super-Client": f"{api_config.get_client_name()}",
+        "Accept-Language": api_config.language,
+    }
+
+    try:
+        with httpx.Client(base_url=base_path, verify=api_config.verify, timeout=api_config.timeout) as client:
+            if params:
+                response = client.get(path, headers=headers, params=params)
+            else:
+                response = client.get(path, headers=headers)
+    except httpx.HTTPError as e:
         print(e)
         hd2api_logger.error(str(e), exc_info=e)
         raise e
